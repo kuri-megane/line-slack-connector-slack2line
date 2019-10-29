e05602974d08231ab851295352ff320b5f9da3f4
kuri.megane my_ubuntu <kuri.megane3060.4@gmail.com>
2019-10-30 00:06:45 +0900

    転送のメッセージ形式を変更

--- plugins/send_line.py
+++ plugins/send_line.py
@@ -6,12 +6,13 @@ from slackbot.bot import listen_to
 from slackbot.bot import respond_to
 
 CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+SEND_CHANNEL_ID = os.environ["LINE_SEND_CHANNEL_ID"]
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 
 # メンションされたメッセージに対して
-@respond_to('(.*)')
+@respond_to('reply (.*)')
 def mention_func(message, _):
 
     # slack投稿者のユーザ名を取得
@@ -21,38 +22,14 @@ def mention_func(message, _):
     text = message.body['text']
     parse_msg = text.split(' ')
 
-    if len(parse_msg) >= 2:
-        to, *send_msg = parse_msg
+    if len(parse_msg) >= 3:
+        _, to, *send_msg = parse_msg
 
         # lineに送る
         line_bot_api.push_message(
-            to=to,
+            to=SEND_CHANNEL_ID,
             messages=TextSendMessage(text=name + ' '.join(map(str, send_msg)))
         )
 
         # メッセージにスタンプをつける
         message.react('+1')
-
-
-# # reply で始まるメッセージに対して
-# @listen_to(r'^reply\s+\S.*')
-# def reaction_func(message):
-#
-#     # slack投稿者のユーザ名を取得
-#     name = message.channel._client.users[message._body['user']]['real_name'] + "さん\n"
-#
-#     # slackに投稿されたメッセージのパース
-#     text = message.body['text']
-#     parse_msg = text.split(' ')
-#
-#     if len(parse_msg) >= 3:
-#         _, to, *send_msg = parse_msg
-#
-#         # lineに送る
-#         line_bot_api.push_message(
-#             to=to,
-#             messages=TextSendMessage(text=name + ' '.join(map(str, send_msg)))
-#         )
-#
-#         # メッセージにスタンプをつける
-#         message.react('+1')
